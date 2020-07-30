2680d7a5599d57215557923486210f7524a8d41a
jdalamo <alamojad@gmail.com>
2020-07-30 15:05:41 -0500

    removed parentheses from function call

--- start.py
+++ start.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     d = Downloader()
     fm = Feh_Manager()
 
-    p1 = Thread(target=d.run())
-    p2 = Thread(target=fm.run())
+    p1 = Thread(target=d.run)
+    p2 = Thread(target=fm.run)
 
     p1.start()
     p2.start()
