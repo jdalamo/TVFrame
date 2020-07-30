3d7ec26f1f993606ef005c861ad09a7f06c9bbe3
jdalamo <alamojad@gmail.com>
2020-07-30 15:02:33 -0500

    changed process to thread

--- start.py
+++ start.py
@@ -1,20 +1,15 @@
-import multiprocessing
 from threading import Thread
 
 from feh_manager import Feh_Manager
 from downloader import Downloader
 
-def main():
+if __name__ == '__main__':
 
     d = Downloader()
     fm = Feh_Manager()
 
     p1 = Thread(target=d.run())
     p2 = Thread(target=fm.run())
-    p1.daemon = True
-    p2.daemon = True
+
     p1.start()
     p2.start()
-
-if __name__ == '__main__':
-    main()
