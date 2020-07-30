020e0e67f79421fb86cb450fd9d008b9965d4a39
jdalamo <alamojad@gmail.com>
2020-07-30 14:45:44 -0500

    changed multiprocessing to multithreading

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from threading import Thread
 
 from feh_manager import Feh_Manager
 from downloader import Downloader
@@ -8,11 +9,11 @@ def main():
     d = Downloader()
     fm = Feh_Manager()
 
-    # p1 = multiprocessing.Process(target=d.run())
-    p2 = multiprocessing.Process(target=fm.run())
-    # p1.daemon = True
+    p1 = Thread(target=d.run())
+    p2 = Thread(target=fm.run())
+    p1.daemon = True
     p2.daemon = True
-    # p1.start()
+    p1.start()
     p2.start()
 
 if __name__ == '__main__':
