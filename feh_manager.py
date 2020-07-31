ecac93d6aa308a5490079954524bd44b0c3c76e7
jdalamo <alamojad@gmail.com>
2020-07-31 15:48:42 -0500

    updated feh_manager to include support for no pictures

--- feh_manager.py
+++ feh_manager.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 import time
 from threading import Thread
 
@@ -8,39 +9,63 @@ class Feh_Manager:
     def __init__(self):
         self.__SETTINGS_PATH = os.path.abspath('settings.json')
         self.__PICS_PATH = os.path.abspath('pics')
+        self.__DEFAULT_IMAGE_PATH = os.path.abspath('default.jpg')
     
     def run(self):
-        print('running')
-        with open(self.__SETTINGS_PATH, 'r') as f:
-            settings = json.load(f)
-
-        last_mode = settings['mode']
-        last_photo = settings['display_photo']
-
-        feh_thread = Thread(target=self.__restart_feh, args=(last_mode, last_photo))
+        try:
+            last_mode, last_photo, pics = self.__update()
+        except json.decoder.JSONDecodeError:
+            last_mode = 'Slideshow'
+            pics = [p for p in os.listdir(self.__PICS_PATH) if os.path.isfile(os.path.join(self.__PICS_PATH, p)) and p != '.DS_Store']
+            if pics:
+                last_photo = random.choice(pics)
+            else:
+                last_photo = self.__DEFAULT_IMAGE_PATH
+        
+        if pics:
+            feh_thread = Thread(target=self.__restart_feh, args=(last_mode, last_photo))
+        else:
+            feh_thread = Thread(target=self.__show_default)
         feh_thread.start()
 
         while True:
             try:
-                with open('settings.json', 'r') as f:
-                    settings = json.load(f)
+                mode, photo, pics = self.__update()
             except json.decoder.JSONDecodeError:
                 time.sleep(1)
                 continue
-
-            mode = settings['mode']
-            photo = settings['display_photo']
-
-            if mode != last_mode:
-                feh_thread = Thread(target=self.__restart_feh, args=(mode, photo))
+            
+            if pics:
+                if mode != last_mode:
+                    feh_thread = Thread(target=self.__restart_feh, args=(mode, photo))
+                    feh_thread.start()
+                if mode == 'Single Photo' and photo != last_photo:
+                    feh_thread = Thread(target=self.__restart_feh, args=(mode, photo))
+                    feh_thread.start()
+            else:
+                if last_mode == 'Default':
+                    continue
+                feh_thread = Thread(target=self.__show_default)
                 feh_thread.start()
-            if mode == 'Single Photo' and photo != last_photo:
-                feh_thread = Thread(target=self.__restart_feh, args=(mode, photo))
-                feh_thread.start()
+                mode = 'Default'
             
             last_mode = mode
             last_photo = photo
             time.sleep(1)
+
+    def __update(self):
+        with open(self.__SETTINGS_PATH, 'r') as f:
+            settings = json.load(f)
+        pics = [p for p in os.listdir(self.__PICS_PATH) if os.path.isfile(os.path.join(self.__PICS_PATH, p)) and p != '.DS_Store']
+
+        mode = settings['mode']
+        photo = settings['display_photo']
+
+        return (mode, photo, pics)
+
+    def __show_default(self):
+        os.system('killall feh')
+        os.system(f'feh {self.__DEFAULT_IMAGE_PATH} -F')
 
     def __restart_feh(self, mode, photo):
         print(self.__PICS_PATH)
